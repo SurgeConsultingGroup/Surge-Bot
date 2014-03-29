471ca23ea0b3565c0c78fa04ebbc4ee48941aa16
Cameron Spear <cameronspear@gmail.com>
2014-03-28 19:33:04 -0700

    Rename runlocally.sh to runlocally-example.sh and add runlocally.sh to .gitignore.

--- runlocally-example.sh
+++ runlocally-example.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# rename this to runlocally.sh so it gets ignored by git
+
+
 export HUBOT_HIPCHAT_JID="USER_ID@chat.hipchat.com"
 export HUBOT_HIPCHAT_PASSWORD="PASSWORD"
 export HUBOT_HIPCHAT_ROOMS="GROUP_ID@conf.hipchat.com"
